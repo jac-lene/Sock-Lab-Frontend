3bd3be7327398d48d4ee4257f128aca7048036f5
jac-lene <jaclenesini@gmail.com>
2022-07-04 15:25:36 -0400

    basic styling setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import DesignPage from './Pages/DesignPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <header className="App-header">
+        <Link to='/'><h1 className='site-title'>sock lab</h1></Link>
+        <Link to='/the-lab'><h2>design</h2></Link>
+        <h2>library</h2>
+        <h2>map</h2>
       </header>
+      <div className='dash-divider'></div>
+      <div className='check-cont'> <div className='checkbord-l'></div>
+      <div className='checkbord-r'></div></div>
+     
+<Routes>
+        {/* <Route path='/app' element={<App users={users}/>} /> */}
+        <Route path="/the-lab" element={<DesignPage/>}/>
+        
+      </Routes>
+
+    
     </div>
   );
 }
---
+++ src/Pages/DesignPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DesignPage.css'
+
+function DesignPage() {
+  return (
+        <div className="main">
+      <header className="App-header">
+        <h1 className='site-title'>sock lab</h1>
+        <h2>change style</h2>
+        <h2>clear</h2>
+        <h2>save</h2>
+      </header>
+      <div className='dash-divider'></div>
+      <body>
+        <h1>hello</h1>
+        </body>
+    </div>
+  )
+}
+
+export default DesignPage
